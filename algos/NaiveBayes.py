b3125294edf2351e946c8e38c12f1313c2e6bc33
Nico Ceresa <ceresanico@gmail.com>
2024-05-27 19:44:20 -0700

    added NaiveBayes file

---
+++ algos/NaiveBayes.py
@@ -0,0 +1,15 @@
+class NaiveBayes:
+    """
+    P(y|X) = (P(X|Y) * P(y))/P(X)
+    
+    X = (x1, x2, ..., xn)
+    P(y) = (P(x1|y) * P(x2|y) * ... * P(xn|y))/p(X)
+    """
+    def __init__(self):
+        pass
+    
+    def fit(self, X, y):
+        pass
+    
+    def predict(self, X):
+        pass
